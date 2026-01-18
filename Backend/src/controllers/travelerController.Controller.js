22d049b5cdf807707747322813bfa2e92cda0808
ghanshyam-du <ghanshyamd816@gmail.com>
2026-01-19 00:25:32 +0530

    added api (Get all buses for logged-in traveler)

--- Backend/src/controllers/travelerController.Controller.js
+++ Backend/src/controllers/travelerController.Controller.js
@@ -24,17 +24,69 @@ export const addBus = asyncHandler(async (req, res, next) => {
         amenities
     } = req.body;
 
-    const traveler = await  Traveler.findOne({user_id: req.user.user_id});
+    const traveler = await Traveler.findOne({ user_id: req.user.user_id });
 
-    if(!traveler) {
+    if (!traveler) {
         return next(new ErrorResponse("Traveler profile not found", 404));
     }
 
-    if(traveler.verification_status !== "APPROVED"){
+    if (traveler.verification_status !== "APPROVED") {
         return next(new ErrorResponse("Your account is not approved yet. Please wait for admin approvel.", 403));
     }
 
-    if(!bus_type || !bus_model || )
+    if (!bus_number || !bus_type || !bus_model || !from_location || !to_location || !total_seats || !fare || !amenities) {
+        return next(new ErrorResponse("Please provide all required fields", 400));
+    }
 
+    const existingBus = await Bus.findOne({ bus_number: bus_number.toUpperCase() });
+    if (existingBus) {
+        return next(new ErrorResponse("Bus number already exists", 400))
+    }
 
+    const bus = await Bus.create({
+        traveler_id: traveler.traveler_id,
+        bus_number: bus_number.toUpperCase(),
+        bus_type,
+        bus_model,
+        from_location,
+        to_location,
+        total_seats,
+        fare,
+        amenities: amenities || []
+    });
+
+    res.status(201).json({
+        success: true,
+        message: "Bus added Successfully",
+        data: Bus
+    });
 })
+
+
+// @desc    Get all buses for logged-in traveler
+// @route   GET /api/traveler/buses
+// @access  Private (Traveler only)
+
+
+export const getMyBuses = asyncHandler(async (req, res, next) => {
+    const traveler = await Traveler.findOne({ user_id: req.user.user_id });
+
+    if (!traveler) {
+        return next(new ErrorResponse("Traveler profile not found", 404));
+    }
+
+    const buses = await Bus.findOne({ traveler_id: traveler.traveler_id }).sort({ createdAt: -1 });
+
+    res.status(200).json({
+        success: true,
+        count: buses.length,
+        data: buses
+    })
+})
+
+
+// @desc    Get single bus details
+// @route   GET /api/traveler/buses/:busId
+// @access  Private (Traveler only)
+
+// export const getBusDetails
