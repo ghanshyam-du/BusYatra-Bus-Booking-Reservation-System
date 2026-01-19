133305cbae74896a6a38a4cbcd6f4413bb26894b
ghanshyam-du <ghanshyamd816@gmail.com>
2026-01-19 14:35:44 +0530

    added the apis of (Get single bus details, update bus)

--- Backend/src/controllers/travelerController.Controller.js
+++ Backend/src/controllers/travelerController.Controller.js
@@ -7,6 +7,7 @@ import SupportTicket from "../models/supportTickets.models.js"
 import asyncHandler from "../utils/asyncHandler.utils.js";
 import ErrorResponse from "../utils/errorResponse.utils.js";
 import mongoose from "mongoose";
+import busScheduleModels from "../models/busSchedule.models.js"
 
 
 // @desc    Add new bus
@@ -89,4 +90,76 @@ export const getMyBuses = asyncHandler(async (req, res, next) => {
 // @route   GET /api/traveler/buses/:busId
 // @access  Private (Traveler only)
 
-// export const getBusDetails
+export const getBusDetails = asyncHandler(async (req, res, next) => {
+    const { busId } = req.params;
+    const traveler = await Traveler.findOne({ user_id: req.user.user_id });
+
+
+    if (!traveler) {
+        return next(new ErrorResponse("Traveler profile not found", 404));
+    }
+
+    const bus = await Bus.findOne({ bus_Id: busId });
+
+    if (!bus) {
+        return next(new ErrorResponse("Bus not found", 404));
+    }
+
+    if (bus.traveler_id !== traveler.traveler_id) {
+        return next(new ErrorResponse("Not authorized to view this bus", 403));
+    }
+
+    const schedules = await BusSchedule.findOne({ bus_id: busId }).sort({ journey_date: -1 })
+
+    res.status(200).json({
+        success: true,
+        data: {
+            bus,
+            schedules_count: schedules.length,
+            upcoming_schedules: schedules.filter(s => new Date(s.journey_date) >= new Date()).length
+        }
+    });
+
+})
+
+
+// @desc    Update bus
+// @route   PUT /api/traveler/buses/:busId
+// @access  Private (Traveler only)
+
+export const updateBus = asyncHandler(async (req, res, next) => {
+    const { busId } = req.params;
+
+    const traveler = await Traveler.findOne({ user_id: req.user.user_id });
+
+    if (!traveler) {
+        return next(new ErrorResponse("Traveler profile not found", 404));
+    }
+
+    const bus = await Bus.findOne({ bus_id: busId });
+
+    if (!bus) {
+        return next(new ErrorResponse("Bus not found", 404));
+    }
+
+    if (bus.traveler_id !== traveler.traveler_id) {
+        return next(new ErrorResponse("Not authorized to view the bus", 403));
+    }
+
+    const allowedUpdates = ['bus_type', 'bus_model', 'fare', 'amenities', 'is_active'];
+    const updates = {};
+
+    allowedUpdates.forEach(field => {
+        if (req.body[field] !== undefined) {
+            updates[field] = req.body[field];
+        }
+    });
+
+    bus = await Bus.findOneAndUpdate({ bus_id: busId }, updates, { new: true, runValidators: true });
+
+    res.status(200).json({
+        success: true,
+        message: "Bus updated successfully",
+        data: bus
+    });
+});
