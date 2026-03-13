279c12ce1cf29eb5889a77cf7be81ab5e47a2e58
ghanshyam-du <ghanshyamd816@gmail.com>
2026-03-13 22:49:43 +0530

    updated

--- Backend/src/models/user.models.js
+++ Backend/src/models/user.models.js
@@ -60,7 +60,7 @@ const userSchema = new mongoose.Schema({
 
 // Hash password before saving
 userSchema.pre('save', async function() {
-  if (!this.isModified('password')) {
+  if (!this.isModified('password ')) {
     return ;
   }
   
